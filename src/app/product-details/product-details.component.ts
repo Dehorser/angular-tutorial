9ba1f2a909733570e492826e41d5a431f5c903ee
Dehorser <happyjoe2008@gmail.com>
2019-12-04 01:41:39 -0800

    Add the ability to add products to a cart

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {products} from '../products';
+import {CartService} from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,6 +14,7 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService,
   ) { }
 
 
@@ -22,4 +24,8 @@ export class ProductDetailsComponent implements OnInit {
     })
   }
 
+  addToCart(product) {
+    window.alert('Your product has been added to the card!');
+    this.cartService.addToCart(product);
+  }
 }
